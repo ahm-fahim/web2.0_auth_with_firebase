4ae9969abae90030d7d1be1de813506b3bb14acd
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-15 14:01:13 +0600

    firebase google signin

--- 01_firebase_basic_setup/src/App.js
+++ 01_firebase_basic_setup/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+} from "firebase/auth";
+import "./App.css";
+import app from "./firebase/firebase.init";
+import React, { useState } from "react";
+
+const auth = getAuth(app);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState({});
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
+            .catch((error) => {
+                console.log("error: ", error);
+            });
+    };
+
+    const handleSignOut = () => {
+        signOut(auth, provider)
+            .then((result) => {
+                setUser({});
+            })
+            .catch((error) => {
+                console.error("error: ", error);
+            });
+    };
+    return (
+        <div className="App">
+            {user.email ? (
+                <button onClick={handleSignOut}>Sign fOut</button>
+            ) : (
+                <button onClick={handleGoogleSignIn}>Google Sing In</button>
+            )}
+
+            {user.email && (
+                <div>
+                    <h1>{user.displayName}</h1>
+                    <p>{user.email}</p>
+                    <img src={user.photoURL} alt="" width="200" height="200" />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
