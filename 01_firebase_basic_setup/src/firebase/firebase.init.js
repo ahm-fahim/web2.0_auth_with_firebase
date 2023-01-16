c3f15d5f4e24749db7da00bef421b1147386a6ff
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-16 19:43:57 +0600

    firebase auth

--- 01_firebase_basic_setup/src/firebase/firebase.init.js
+++ 01_firebase_basic_setup/src/firebase/firebase.init.js
@@ -4,6 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBIg8OEAe2HA6XTfWrvVJrkFdpA7GLqzMU",
+    authDomain: "fir-basic-setup-eee6d.firebaseapp.com",
+    projectId: "fir-basic-setup-eee6d",
+    storageBucket: "fir-basic-setup-eee6d.appspot.com",
+    messagingSenderId: "110093615142",
+    appId: "1:110093615142:web:fe1c820bd95c245e97e6e7",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
