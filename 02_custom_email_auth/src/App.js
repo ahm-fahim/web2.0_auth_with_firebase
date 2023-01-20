c414b271749797bb83ac04ed10ec55013b6e48ce
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-20 22:33:18 +0600

    form input output submit

--- 02_custom_email_auth/src/App.js
+++ 02_custom_email_auth/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { getAuth } from "firebase/auth";
 import app from "./firebase/firebase.init";
@@ -6,7 +5,25 @@ import app from "./firebase/firebase.init";
 const auth = getAuth(app);
 
 function App() {
-    return <div className="App"></div>;
+    const handleRegister = (event) => {
+        event.preventDefault();
+      console.log(event.target.email.value);
+        console.log(event.target.password.value);
+      
+    };
+    return (
+        <div className="App">
+            <form onSubmit={handleRegister}>
+                <input type="email" name="email" placeholder="your email" />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="your password"
+                />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
