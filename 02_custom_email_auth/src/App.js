cb90c1967dad2a370f624e79aed31e68ace550b8
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-21 18:26:13 +0600

    firebase custom email password signup

--- 02_custom_email_auth/src/App.js
+++ 02_custom_email_auth/src/App.js
@@ -1,29 +1,46 @@
 import "./App.css";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "./firebase/firebase.init";
-import {Form,Button }from "react-bootstrap";
-
+import { Form, Button } from "react-bootstrap";
 
 const auth = getAuth(app);
 
 function App() {
     const handleRegister = (event) => {
         event.preventDefault();
-        console.log(event.target.email.value);
-        console.log(event.target.password.value);
-      
+        let email = event.target.email.value;
+        let password = event.target.password.value;
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
     };
     return (
         <div className="container d-flex justify-content-center mt-5">
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        placeholder="Enter email"
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                    />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Register
