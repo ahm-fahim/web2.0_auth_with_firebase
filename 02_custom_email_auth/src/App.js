57fff0bf8046083e4ce20e8a41269d726b906483
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-21 16:03:26 +0600

    react bootstrap form

--- 02_custom_email_auth/src/App.js
+++ 02_custom_email_auth/src/App.js
@@ -1,27 +1,34 @@
 import "./App.css";
 import { getAuth } from "firebase/auth";
 import app from "./firebase/firebase.init";
+import {Form,Button }from "react-bootstrap";
+
 
 const auth = getAuth(app);
 
 function App() {
     const handleRegister = (event) => {
         event.preventDefault();
-      console.log(event.target.email.value);
+        console.log(event.target.email.value);
         console.log(event.target.password.value);
       
     };
     return (
-        <div className="App">
-            <form onSubmit={handleRegister}>
-                <input type="email" name="email" placeholder="your email" />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="your password"
-                />
-                <button type="submit">Register</button>
-            </form>
+        <div className="container d-flex justify-content-center mt-5">
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
         </div>
     );
 }
