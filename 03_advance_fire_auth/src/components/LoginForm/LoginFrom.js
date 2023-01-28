0b1c0c8d9c9a614695245741d345618dfc3a60c5
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-28 14:25:36 +0600

    login form design done

--- 03_advance_fire_auth/src/components/LoginForm/LoginFrom.js
+++ 03_advance_fire_auth/src/components/LoginForm/LoginFrom.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LoginFrom = () => {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const pass = form.password.value;
+        console.log(email, pass);
+    };
     return (
-        <div>
-            Login
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        In this project we learn about react router , firebase ,
+                        tailwind , daisy UI, Iconscout! So let's go!
+                    </p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                name="email"
+                                type="email"
+                                placeholder="email"
+                                className="input input-bordered"
+                                required
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="password"
+                                className="input input-bordered"
+                                required
+                            />
+                            <label className="label">
+                                <a
+                                    href="#"
+                                    className="label-text-alt link link-hover text-green-500"
+                                >
+                                    Forgot password?
+                                </a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type="submit" className="btn btn-success">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
