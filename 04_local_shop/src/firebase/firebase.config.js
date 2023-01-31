17d6a911986ee94b880bfc05a2c0eb4e3b4b8401
mdFahimMorshed <contactahmfahim@gmail.com>
2023-01-31 20:04:19 +0600

    firebase setup done

---
+++ 04_local_shop/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBqMuLkvCZ36Lh4Lvr559pKOWTYfNJ3bh8",
+    authDomain: "local-shop-a7b0a.firebaseapp.com",
+    projectId: "local-shop-a7b0a",
+    storageBucket: "local-shop-a7b0a.appspot.com",
+    messagingSenderId: "1092348131523",
+    appId: "1:1092348131523:web:3fcfe93f30bade00d90a76",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
