c85670a3bf58fd98dcff94ecac3d9805426c1807
mdFahimMorshed <contactahmfahim@gmail.com>
2023-02-02 20:02:19 +0600

    firebase auth

--- 04_local_shop/src/firebase/firebase.config.js
+++ 04_local_shop/src/firebase/firebase.config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
     apiKey: "AIzaSyBqMuLkvCZ36Lh4Lvr559pKOWTYfNJ3bh8",
     authDomain: "local-shop-a7b0a.firebaseapp.com",
