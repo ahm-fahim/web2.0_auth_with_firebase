38afeb83afa0080c0fbf55dde267f673b785f295
mdFahimMorshed <contactahmfahim@gmail.com>
2023-02-08 20:54:35 +0600

    relax

--- 04_local_shop/src/components/Register/Register.js
+++ 04_local_shop/src/components/Register/Register.js
@@ -5,7 +5,7 @@ const Register = () => {
         <div>
             <h1>Register</h1>
             <h1>I am relaxing now</h1>
-            
+            <h2>Today I also be relaxing</h2>
         </div>
     );
 };
