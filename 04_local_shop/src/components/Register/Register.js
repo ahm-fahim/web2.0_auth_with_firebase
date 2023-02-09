b0c6b00b2ebfb7066d27bdedbd2d7a9e8ddf54b4
mdFahimMorshed <contactahmfahim@gmail.com>
2023-02-09 20:59:13 +0600

    have a relax

--- 04_local_shop/src/components/Register/Register.js
+++ 04_local_shop/src/components/Register/Register.js
@@ -6,6 +6,7 @@ const Register = () => {
             <h1>Register</h1>
             <h1>I am relaxing now</h1>
             <h2>Today I also be relaxing</h2>
+            <h3>Relaxing also today</h3>
         </div>
     );
 };
