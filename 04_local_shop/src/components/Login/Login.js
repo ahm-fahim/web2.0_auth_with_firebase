23d023eae95c49512c1bcce8fad777873f38f2ed
mdFahimMorshed <contactahmfahim@gmail.com>
2023-02-14 21:04:00 +0600

    login to study

--- 04_local_shop/src/components/Login/Login.js
+++ 04_local_shop/src/components/Login/Login.js
@@ -3,11 +3,7 @@ import React from "react";
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
-            <h2>This is login form</h2>
-            <h1>Last day of relax</h1>
-            <h1>I Dont know the end of relax.</h1>
-            <p>I am tired</p>
+        <h1>Let's login to study!</h1>
         </div>
     );
 };
