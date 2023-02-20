e75336e2386521a68d3412ed7c0e630cf8313e78
mdFahimMorshed <contactahmfahim@gmail.com>
2023-02-20 20:42:28 +0600

    user context

--- 04_local_shop/src/contexts/UserContext.js
+++ 04_local_shop/src/contexts/UserContext.js
@@ -3,7 +3,8 @@ import React, { createContext } from "react";
 export const AuthContext = createContext();
 
 const UserContext = ({ children }) => {
-    const user = { email: "aga" };
+    const user = { email: "aga", pass: "1231if" };
+    const user2 = { email: "ewi" };
     const authInfo = { user };
     return (
         <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
